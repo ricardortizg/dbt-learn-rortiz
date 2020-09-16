2933fdabf910fd2ed6c204e79e0075eda8d0900d
Ricardo Ortiz <rortizg@live.com>
2020-09-16 17:39:31 +0000

    update_afternoon

--- models/orders.sql
+++ models/orders.sql
@@ -1,12 +1,35 @@
 
-with all_orders as (
 
+
+with all_orders as (
     select * from {{ ref('stg_orders') }}
+),
+
+all_payments as (
+    select * from {{ ref('stg_payments') }}
 
 ),
 
-payments as (
+valid_payments as (
+    select * from all_payments
+    where status = 'success'
 
-    select * from {{ ref('stg_payments') }}
- 
+),
+
+order_payments as (
+
+    select
+        all_orders.order_id,
+        all_orders.customer_id,
+        sum(valid_payments.payment) 
+    from all_orders
+    left outer join valid_payments
+        on all_orders.order_id = valid_payments.order_id
+        group by 1, 2
 )
+
+select * from order_payments
+
+
+
+
